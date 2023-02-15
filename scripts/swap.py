945194b43fb53b53ed5970c42326d67e31bfb101
Ssali Benjamin <ssalibenjamin0402@gmail.com>
2023-02-16 00:18:38 +0300

    Using linter

--- scripts/swap.py
+++ scripts/swap.py
@@ -1,5 +1,5 @@
 from scripts.helpful_scripts import get_account, deploy
-from brownie import interface, Gencoin
+from brownie import interface
 from web3 import Web3
 
 amount = Web3.toWei(10, "ether")
@@ -10,7 +10,8 @@ def main():
     account = get_account()
     mainsafe = deploy()
     #mainsafe.addUser("ssalibenjamin@gmail.com", Gencoin[0].address, {"from":account})
+    # type: ignore 
     userAddress = mainsafe.emailToUser("ssalibenjamin@gmail.com")
-    user = interface.IUser(userAddress)
+   user = interface.IUser(userAddress)
     tx = user.deposit("USDT",amount,"ssalibenjamin@gmail.com", {"from":account})
     tx = user.swap("ssalibenjamin@gmail.com", "USDT", "DAI", value, {"from":account})
